baf69f526e7fb7c8c3e6980ae79eb69c062de669
mengxianghan <1056811341@qq.com>
2020-06-26 09:11:04 +0800

    新增 storage 工具类

---
+++ utils/storage.js
@@ -0,0 +1,24 @@
+let storage = {}
+
+const storageMap = ['value1', 'value2']
+
+storageMap.forEach((item, index) => {
+    let name = item.charAt(0).toUpperCase() + item.slice(1)
+    storage[`set${name}`] = (value) => {
+        return uni.setStorageSync(item, value)
+    }
+
+    storage[`get${name}`] = () => {
+        return uni.getStorageSync(item)
+    }
+
+    storage[`remove${name}`] = () => {
+        return uni.removeStorageSync(item)
+    }
+
+    storage[`clear${name}`] = () => {
+        return uni.clearStorageSync(item)
+    }
+})
+
+module.exports = storage
