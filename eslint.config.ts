e8b590b42640f0503b8da6efac160576757619ef
mengxianghan <1056811341@qq.com>
2025-05-11 14:31:48 +0800

    feat: 添加 Vue 组件相关 ESLint 规则

--- eslint.config.ts
+++ eslint.config.ts
@@ -18,4 +18,14 @@ export default antfu({
   },
   vue: true,
   typescript: true,
+  rules: {
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: { max: 1 },
+        multiline: { max: 1 },
+      },
+    ],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+  },
 })
