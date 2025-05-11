67d2dc79580db74e47613da6771f97df3859d600
mengxianghan <1056811341@qq.com>
2025-05-11 14:31:15 +0800

    style: 格式化代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,10 @@
 
 <template>
   <view class="h-svh flex flex-col items-center">
-    <image src="/static/logo.svg" class="w-[120px]" />
+    <image
+      src="/static/logo.svg"
+      class="w-[120px]"
+    />
     <view>
       欢迎使用 uniapp template
     </view>
