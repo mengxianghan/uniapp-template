0c7010dce457da6480de0aecea92e1b24b596365
mengxianghan <1056811341@qq.com>
2025-05-23 15:12:28 +0800

    refactor: 优化上传脚本

--- scripts/mp-weixin/upload.js
+++ scripts/mp-weixin/upload.js
@@ -1,23 +1,21 @@
 const consola = require('consola')
-const { ci, project, baseSetting } = require('./config')
-const { log } = require('./log')
-const { version } = require('../utils')
+const {ci, project, baseSetting} = require('./config')
+const {log} = require('./log')
+const {version} = require('../utils')
 
-  (async () => {
-    consola.start('正在上传…')
-    const result = await ci.upload({
-      project,
-      version: version,
-      desc: `环境：${process.env.APP_ENV}；版本：v${version}`,
-      setting: {
-        ...baseSetting,
-      },
-      onProgressUpdate: console.log,
-      robot: 1,
-    })
+consola.start('正在上传…')
+ci.upload({
+  project,
+  version: version,
+  desc: `环境：${process.env.APP_ENV}；版本：v${version}`,
+  setting: {
+    ...baseSetting,
+  },
+  onProgressUpdate: console.log,
+  robot: 1,
+}).then((result) => {
+  log(result)
+  consola.success('上传完成')
 
-    log(result)
-    consola.success('上传完成')
-
-    process.exit()
-  })()
+  process.exit()
+})
