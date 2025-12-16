edbc5dbb7582c06a39c63d016a411f8bf4032821
孟祥涵 <1056811341@qq.com>
2025-12-16 09:54:01 +0800

    feat(utils): 添加UI工具函数

---
+++ src/utils/ui.ts
@@ -0,0 +1,29 @@
+export function showToast(options: UniApp.ShowToastOptions | string) {
+  options = typeof options === 'string' ? { title: options } : options
+  uni.showToast({
+    duration: 1500,
+    mask: true,
+    icon: 'none',
+    ...options,
+  })
+}
+
+export const hideToast = uni.hideToast
+
+export function showModal(options: UniApp.ShowModalOptions | string) {
+  options = typeof options === 'string' ? { content: options } : options
+  uni.showModal({
+    confirmColor: '#FF8484',
+    ...options,
+  })
+}
+
+export function showLoading(options?: UniApp.ShowLoadingOptions | string) {
+  options = typeof options === 'string' ? { title: options } : options
+  uni.showLoading({
+    mask: true,
+    ...(options || {}),
+  })
+}
+
+export const hideLoading = uni.hideLoading
