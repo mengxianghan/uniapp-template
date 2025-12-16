645bce23273dd72975ab7a82e6c8768ad231cda8
孟祥涵 <1056811341@qq.com>
2025-12-16 09:55:12 +0800

    feat(utils): 添加多种类型判断工具函数

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -2,7 +2,7 @@ export function isNumber(value: unknown): value is number {
   return typeof value === 'number' && !isNaN(value)
 }
 
-export function isDef(value: unknown): boolean {
+export function isDef(value: unknown): value is NonNullable<unknown> {
   return value !== undefined && value !== null
 }
 
@@ -12,4 +12,32 @@ export function isDate(value: unknown): value is Date {
 
 export function isNaN(value: unknown): boolean {
   return Number.isNaN(value)
+}
+
+export function isEmpty(value: unknown): value is '' | null | undefined {
+  return isEmptyString(value) || value === null || value === undefined
+}
+
+export function isEmptyString(value: unknown): value is '' {
+  return value === ''
+}
+
+export function isPromise<T = any>(value: unknown): value is Promise<T> {
+  return isObject(value) && isFunction(value.then) && isFunction(value.catch)
+}
+
+export function isFunction<T extends (...args: any[]) => any>(value: unknown): value is T {
+  return typeof value === 'function'
+}
+
+export function isObject(value: unknown): value is Record<any, any> {
+  return value !== null && typeof value === 'object'
+}
+
+export function isString(value: unknown): value is string {
+  return Object.prototype.toString.call(value) === '[object String]'
+}
+
+export function isPhoneNumber(value: string): boolean {
+  return /^1[3-9]\d{9}$/.test(value)
 }
