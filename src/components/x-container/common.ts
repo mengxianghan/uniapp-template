3ffb51ee2ffa690ad4e708026748409de457fb1d
孟祥涵 <1056811341@qq.com>
2025-12-28 20:17:30 +0800

    feat(x-container): 添加导航栏和标签栏配置选项

--- src/components/x-container/common.ts
+++ src/components/x-container/common.ts
@@ -2,6 +2,8 @@ export interface ContainerProps {
   customClass?: string
   bodyClass?: string
   backgroundImage?: string
+  hasNavBar?: boolean
+  hasTabBar?: boolean
 }
 
 export const defaultContainerProps = {}
--- src/components/x-container/x-container.vue
+++ src/components/x-container/x-container.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import type { ContainerProps } from './common'
-import { useSlots } from 'vue'
+import { computed, type CSSProperties, useSlots } from 'vue'
 import { clsx } from '@/utils'
 import { defaultContainerProps } from './common'
+
+defineOptions({
+  name: 'XContainer',
+  options: {
+    virtualHost: true,
+  },
+})
 
 withDefaults(
   defineProps<ContainerProps>(),
@@ -10,18 +17,30 @@ withDefaults(
 )
 
 const slots = useSlots()
+
+const rootStyle = computed(() => {
+  const style: CSSProperties = {}
+
+  const { windowHeight } = uni.getSystemInfoSync()
+
+  style.minHeight = `${windowHeight}px`
+
+  return style
+})
 </script>
 
 <template>
   <view
     :class="clsx(
-      'min-h-screen',
+      'relative',
       customClass,
     )"
+    :style="rootStyle"
   >
+    <!-- 背景图 -->
     <image
       v-if="backgroundImage"
-      class="fixed left-0 top-0 w-full"
+      class="fixed left-0 top-0 w-full pointer-events-none"
       :src="backgroundImage"
       mode="widthFix"
     />
@@ -29,7 +48,10 @@ const slots = useSlots()
       v-if="slots.header"
       class="z-[9999] relative"
     >
-      <slot name="header" />
+      <slot
+        name="header"
+        :background-image="backgroundImage"
+      />
     </view>
 
     <view
